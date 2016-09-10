158b26f4db98868c606d0282cfb2f773d929d552
Nicolas Frankel <nicolas@frankel.ch>
2016-09-10 13:25:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+#Type-safe annotations
+
+Just use. `Nuff said.
+
+```java
+@SuppressWarningsAuxilliaryClass
+@SuppressWarningsCast
+@SuppressWarningsClassFile
+@SuppressWarningsDepAnn
+@SuppressWarningsDeprecation
+@SuppressWarningsDivZero
+@SuppressWarningsEmpty
+@SuppressWarningsFallthrough
+@SuppressWarningsFinally
+@SuppressWarningsOptions
+@SuppressWarningsOverloads
+@SuppressWarningsOverrides
+@SuppressWarningsPath
+@SuppressWarningsProcessing
+@SuppressWarningsRawTypes
+@SuppressWarningsSerial
+@SuppressWarningsStatic
+@SuppressWarningsTry
+@SuppressWarningsUnchecked
+@SuppressWarningsVarargs
+@SuppressWarningsAll
+``` 
---
+++ src/main/kotlin/ch/frankel/safeannotation/SuppressWarning.kt
@@ -0,0 +1,99 @@
+/*
+ * (C) Copyright 2016 Nicolas Fränkel (https://blog.frankel.ch/me).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.frankel.safeannotation
+
+@SuppressWarnings("auxiliaryclass")
+public annotation class SuppressWarningsAuxilliaryClass
+
+@SuppressWarnings("cast")
+public annotation class SuppressWarningsCast
+
+@SuppressWarnings("classfile")
+public annotation class SuppressWarningsClassFile
+
+@SuppressWarnings("dep-ann")
+public annotation class SuppressWarningsDepAnn
+
+@SuppressWarnings("deprecation")
+public annotation class SuppressWarningsDeprecation
+
+@SuppressWarnings("divzero")
+public annotation class SuppressWarningsDivZero
+
+@SuppressWarnings("empty")
+public annotation class SuppressWarningsEmpty
+
+@SuppressWarnings("fallthrough")
+public annotation class SuppressWarningsFallthrough
+
+@SuppressWarnings("finally")
+public annotation class SuppressWarningsFinally
+
+@SuppressWarnings("options")
+public annotation class SuppressWarningsOptions
+
+@SuppressWarnings("overloads")
+public annotation class SuppressWarningsOverloads
+
+@SuppressWarnings("overrides")
+public annotation class SuppressWarningsOverrides
+
+@SuppressWarnings("path")
+public annotation class SuppressWarningsPath
+
+@SuppressWarnings("processing")
+public annotation class SuppressWarningsProcessing
+
+@SuppressWarnings("rawtypes")
+public annotation class SuppressWarningsRawTypes
+
+@SuppressWarnings("serial")
+public annotation class SuppressWarningsSerial
+
+@SuppressWarnings("static")
+public annotation class SuppressWarningsStatic
+
+@SuppressWarnings("try")
+public annotation class SuppressWarningsTry
+
+@SuppressWarnings("unchecked")
+public annotation class SuppressWarningsUnchecked
+
+@SuppressWarnings("varargs")
+public annotation class SuppressWarningsVarargs
+
+@SuppressWarningsAuxilliaryClass
+@SuppressWarningsCast
+@SuppressWarningsClassFile
+@SuppressWarningsDepAnn
+@SuppressWarningsDeprecation
+@SuppressWarningsDivZero
+@SuppressWarningsEmpty
+@SuppressWarningsFallthrough
+@SuppressWarningsFinally
+@SuppressWarningsOptions
+@SuppressWarningsOverloads
+@SuppressWarningsOverrides
+@SuppressWarningsPath
+@SuppressWarningsProcessing
+@SuppressWarningsRawTypes
+@SuppressWarningsSerial
+@SuppressWarningsStatic
+@SuppressWarningsTry
+@SuppressWarningsUnchecked
+@SuppressWarningsVarargs
+public annotation class SuppressWarningsAll
